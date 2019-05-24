69700624b2a0ba9bc57f24345f2d47e0f3763b96
tum0xa <timofey.samodurov@mail.ru>
2019-05-24 11:45:18 +0500

    Added the solution for the task #6 of the Lesson 1

--- Lesson_1/6.py
+++ Lesson_1/6.py
@@ -1,1 +1,16 @@
 # 6.	Пользователь вводит номер буквы в алфавите. Определить, какая это буква.
+
+import string
+
+alphabet = list(string.ascii_lowercase)
+
+pos_of_letter = input("Type the position of a letter in the english alphabet: ")
+
+if pos_of_letter.isdecimal():
+    pos_of_letter = int(pos_of_letter)
+    if pos_of_letter < 0 or pos_of_letter > len(alphabet):
+        print("Type the correct number of position (from 1 to 26), please!")
+        quit(0)
+    print(f"Your letter is '{alphabet[pos_of_letter - 1].upper()}'.")
+else:
+    print("Invalid input!")
