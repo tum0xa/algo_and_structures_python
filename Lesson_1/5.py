bbf92d7502d8d6e90b590245ea5038705ffc33f0
tum0xa <timofey.samodurov@mail.ru>
2019-05-24 11:38:52 +0500

    Added the solution for the task #5 of the Lesson 1

--- Lesson_1/5.py
+++ Lesson_1/5.py
@@ -1,2 +1,23 @@
-#5.	Пользователь вводит две буквы. Определить, на каких местах
+# 5.	Пользователь вводит две буквы. Определить, на каких местах
 # алфавита они стоят, и сколько между ними находится букв.
+
+import string
+
+alphabet = list(string.ascii_lowercase)
+
+first_letter = input("Type a first letter: ")
+second_letter = input("Type a second letter: ")
+
+if len(first_letter) == 1 and len(second_letter) == 1 \
+        and alphabet.count(first_letter) == 1 and alphabet.count(second_letter) == 1:
+    print(f"Position of the letter '{first_letter}' is {alphabet.index(first_letter) + 1}.")
+    print(f"Position of the letter '{second_letter}' is {alphabet.index(second_letter) + 1}.")
+
+    if second_letter < first_letter:
+        temp_value = first_letter
+        first_letter = second_letter
+        second_letter = temp_value
+
+    print(f"Letters between of them: {alphabet.index(second_letter) - alphabet.index(first_letter)}.")
+else:
+    print("Invalid input!")
