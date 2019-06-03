efe5da4865a49b11fe8646cc00f0b6fb54e30efd
tum0xa <timofey.samodurov@mail.ru>
2019-06-03 22:18:05 +0500

    Added solution for the task 7 of the Lesson 2

--- Lesson_2/7.py
+++ Lesson_2/7.py
@@ -3,3 +3,21 @@
 натуральных чисел выполняется равенство: 1+2+...+n = n(n+1)/2,
  где n - любое натуральное число.
 """
+
+
+# recursion
+
+def sum_of_num(num):
+    if num == 0:
+        return 0
+    else:
+        return num + sum_of_num(num - 1)
+
+
+n = input("Type a natural number: ")
+if n.isdecimal() and int(n) > 0:
+    n = int(n)
+    if sum_of_num(n) == n * (n + 1) / 2:
+        print(f"The equality 1+2+...+{n} = {n}*({n}+1)/2 is fulfilled!")
+else:
+    print("Invalid input")
