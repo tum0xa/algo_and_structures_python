3e0782f4c76a7db61b848d709af6bb0f3fe7b13b
tum0xa <timofey.samodurov@mail.ru>
2019-06-03 22:08:56 +0500

    Added solution for the task 4 of the Lesson 2

--- Lesson_2/4.py
+++ Lesson_2/4.py
@@ -2,3 +2,20 @@
 4.	Найти сумму n элементов следующего ряда чисел: 1 -0.5 0.25 -0.125 ...
 Количество элементов (n) вводится с клавиатуры.
 """
+
+
+# recursion
+
+
+def sum_of_row(num, value=1.0):
+    if num > 1:
+        return value + sum_of_row(num - 1, value - value * 1.5)
+    else:
+        return value
+
+
+n = input("Type the quantity of elements in the sequence (1 -0.5 0.25 -0.125 ...): ")
+if n.isdecimal():
+    print(f"Sum of the sequence's elements equals to {sum_of_row(int(n))}")
+else:
+    print("Invalid input!")
