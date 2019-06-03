b4765a269e4512f69f2edd6d9a696d11f60806c5
tum0xa <timofey.samodurov@mail.ru>
2019-06-03 22:07:02 +0500

    Added solution for the task 3 of the Lesson 2

--- Lesson_2/3.py
+++ Lesson_2/3.py
@@ -4,3 +4,24 @@
  то надо вывести число 6843.
 """
 
+
+# recursion
+
+
+def invert_number(num):
+    """
+
+    :param num: Number that will be inverted
+    :return: Inverted number
+    """
+    if int(num) != 0:
+        return num % 10 * (10 ** (len(str(num)) - 1)) + invert_number(num // 10)
+    else:
+        return False
+
+
+n = input("Type a number: ")
+if n.isdecimal() and int(n) > 0:
+    print(f"Your reverse number is {invert_number(int(n))}.")
+else:
+    print("Invalid input!")
