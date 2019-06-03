ae46793ac29dbbd3899198c30323fa703e5007bc
tum0xa <timofey.samodurov@mail.ru>
2019-06-03 22:17:07 +0500

    Added solution for the task 6 of the Lesson 2

--- Lesson_2/6.py
+++ Lesson_2/6.py
@@ -5,3 +5,28 @@
 число, чем то, что загадано. Если за 10 попыток число не отгадано,
 то вывести загаданное число.
 """
+
+import random
+
+random_number = random.randint(0, 100)
+
+for i in range(0, 10):
+    user_number = input("Try guess the number (from 0 to 100): ")
+    if user_number.isdecimal() and (int(user_number) > 100 or int(user_number) < 0):
+        print("Invalid input! Try again.")
+        continue
+    else:
+        if int(user_number) == random_number:
+            print("Congratulations! You guessed the number!")
+            quit()
+        elif int(user_number) > random_number:
+            print("The hidden number is less!")
+            continue
+        elif int(user_number) < random_number:
+            print("The hidden number is greater!")
+            continue
+        else:
+            print("Something wrong!? Try again.")
+            continue
+
+print(f"So sad! The number is {random_number}!")
