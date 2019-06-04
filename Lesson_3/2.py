81c521dbaaf5844b5fde9608a58a153cd6976102
tum0xa <timofey.samodurov@mail.ru>
2019-06-04 23:20:26 +0500

    Added solution for the task 2 of the Lesson 3

--- Lesson_3/2.py
+++ Lesson_3/2.py
@@ -5,3 +5,16 @@
 (или 0, 3, 4, 5 - если индексация начинается с нуля),
 т.к. именно в этих позициях первого массива стоят четные числа.
 """
+
+input_list = input("Type the list of numbers divided by spaces: ").split()
+result_list = []
+for pos, num in enumerate(input_list):
+    if num.isdecimal():
+        if int(num) % 2 == 0:
+            result_list.append(pos)
+            continue
+        else:
+            if input_list.index(num) == len(input_list) - 1:
+                result_list = None
+
+print(f"The position(s) of even number(s) in the list: {result_list}")
