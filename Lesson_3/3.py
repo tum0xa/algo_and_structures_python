2fab4443c7ef8030ba15a8d68c31a715ca9ee7aa
tum0xa <timofey.samodurov@mail.ru>
2019-06-04 23:42:10 +0500

    Added solution for the task 3 of the Lesson 3

--- Lesson_3/3.py
+++ Lesson_3/3.py
@@ -1,2 +1,26 @@
-#3.	В массиве случайных целых чисел поменять местами минимальный
+# 3.	В массиве случайных целых чисел поменять местами минимальный
 # и максимальный элементы.
+
+
+from random import randint
+
+random_list = []
+for i in range(3, randint(3, 20)):
+    random_list.append(randint(1, 100))
+
+random_list = list(set(random_list))
+
+big, small = 0, 0
+for pos, num in enumerate(random_list):
+    if num < small or pos == 0:
+        small = num
+    if num > big:
+        big = num
+
+result_list = random_list.copy()
+result_list[random_list.index(big)], result_list[random_list.index(small)] = \
+    result_list[random_list.index(small)], result_list[random_list.index(big)]
+
+print(f"The biggest number is {big} and the smaller is {small}")
+print(f"The original random list is {random_list}")
+print(f"The result list is {result_list}")
