27d4388f05c3d8add00947eb0d682554832cef00
tum0xa <timofey.samodurov@mail.ru>
2019-06-08 01:52:00 +0500

    Added solution for the task 3 of the Lesson 7

--- Lesson_7/3.py
+++ Lesson_7/3.py
@@ -6,3 +6,42 @@
 массива. Но если это слишком сложно, то используйте метод сортировки,
  который не рассматривался на уроках
 """
+
+from random import randint
+
+m = 5
+
+random_array = [randint(0, 100) for _ in range(2 * m + 1)]
+
+print(f"The array is: {random_array}")
+
+median = 0
+for num in random_array:
+    greater = 0
+    less = 0
+    for i in range(len(random_array)):
+        if num > random_array[i]:
+            greater += 1
+        if num < random_array[i]:
+            less += 1
+        if num == random_array[i]:
+            continue
+
+        if greater > m or less > m:
+            break
+
+    # print(num, greater, less)
+
+    if greater == m or less == m:
+        median = num
+
+print(f"Your median is {median}!")
+
+# Выводим отсортированный массив с выделенной медианой для наглядности
+random_array.sort()
+for pos,item in enumerate(random_array):
+    if pos == m:
+        print(f'"{item}"', end=' ')
+
+    else:
+        print(item, end=' ')
