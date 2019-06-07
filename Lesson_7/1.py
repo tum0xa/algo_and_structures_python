2e7b17bed680c9e88b482fa8dfb4c1aa93b0bb82
tum0xa <timofey.samodurov@mail.ru>
2019-06-07 22:31:48 +0500

    Added solution for the task 1 of the Lesson 7

--- Lesson_7/1.py
+++ Lesson_7/1.py
@@ -4,3 +4,24 @@
 исходный и отсортированный массивы. Сортировка должна быть реализована в
 виде функции. По возможности доработайте алгоритм (сделайте его умнее).
 """
+
+from random import randint
+
+
+def bubble_sort(array):
+    step = 0
+    while step < len(array):
+        for i in range(len(array) - step):
+            if i < len(array) - step - 1:
+                if array[i] > array[i + 1]:
+                    array[i + 1], array[i] = array[i], array[i + 1]
+        step += 1
+    return array
+
+
+SIZE_OF_ARRAY = 20
+
+unsort_array = [randint(-100, 100) for i in range(SIZE_OF_ARRAY)]
+
+print(f"Unsort array: {unsort_array}")
+print(f"Sort array: {bubble_sort(unsort_array)}")
