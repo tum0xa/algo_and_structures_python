d82d409dd5e26e5816d9e6502ad302aef7766f68
tum0xa <timofey.samodurov@mail.ru>
2019-06-08 14:50:38 +0500

    Added info about using memory by the task 1 of the Lesson 7

--- Lesson_7/1.py
+++ Lesson_7/1.py
@@ -4,10 +4,10 @@
 исходный и отсортированный массивы. Сортировка должна быть реализована в
 виде функции. По возможности доработайте алгоритм (сделайте его умнее).
 """
-
+from memory_profiler import profile
 from random import randint
 
-
+@profile
 def bubble_sort(array):
     step = 0
 
@@ -32,3 +32,24 @@ unsort_array = [randint(-100, 100) for i in range(SIZE_OF_ARRAY)]
 
 print(f"Unsort array: {unsort_array}")
 print(f"Sort array: {bubble_sort(unsort_array)}")
+
+
+# Line #    Mem usage    Increment   Line Contents
+# ================================================
+# 10     35.5 MiB     35.5 MiB   @profile
+# 11                             def bubble_sort(array):
+# 12     35.5 MiB      0.0 MiB       step = 0
+# 13
+# 14     35.5 MiB      0.0 MiB       while step < len(array):
+# 15     35.5 MiB      0.0 MiB           exchange = False
+# 16     35.5 MiB      0.0 MiB           for i in range(len(array) - step):
+# 17     35.5 MiB      0.0 MiB               if i < len(array) - step - 1:
+# 18     35.5 MiB      0.0 MiB                   if array[i] > array[i + 1]:
+# 19     35.5 MiB      0.0 MiB                       array[i + 1], array[i] = array[i], array[i + 1]
+# 20     35.5 MiB      0.0 MiB                       exchange = True
+# 21     35.5 MiB      0.0 MiB           if not exchange:
+# 22     35.5 MiB      0.0 MiB               break
+# 23                                     else:
+# 24     35.5 MiB      0.0 MiB               step += 1
+# 25                                 # print(f"Quantity of a sort steps: {step}")
+# 26     35.5 MiB      0.0 MiB       return array
